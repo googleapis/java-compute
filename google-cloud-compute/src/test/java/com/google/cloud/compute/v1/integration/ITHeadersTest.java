633ecc3117fe29d94d2affaac7f542c260d1a897
Georgy Ekkert <gekkert@griddynamics.com>
2021-10-29 15:43:41 -0700

    tests: formatiing

--- google-cloud-compute/src/test/java/com/google/cloud/compute/v1/integration/ITHeadersTest.java
+++ google-cloud-compute/src/test/java/com/google/cloud/compute/v1/integration/ITHeadersTest.java
@@ -1,8 +1,5 @@
 package com.google.cloud.compute.v1.integration;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-
 import com.google.cloud.compute.v1.Address;
 import com.google.cloud.compute.v1.AddressesClient;
 import com.google.cloud.compute.v1.AddressesSettings;
@@ -10,6 +7,8 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -17,43 +16,42 @@ import org.junit.Test;
 
 public class ITHeadersTest {
 
-    private static HttpServer server;
-    private static AddressesClient addressesClient;
-    private static Headers headers;
+  private static HttpServer server;
+  private static AddressesClient addressesClient;
+  private static Headers headers;
 
+  @BeforeClass
+  public static void setUp() throws IOException {
+    server = HttpServer.create(new InetSocketAddress("127.0.0.1", 0), 0);
+    server.createContext("/", new MyHandler());
+    server.setExecutor(null);
+    server.start();
+    String address = server.getAddress().toString().replace("/", "http://");
+    AddressesSettings addressesSettings =
+        AddressesSettings.newBuilder().setEndpoint(address).build();
+    addressesClient = AddressesClient.create(addressesSettings);
+  }
 
-    @BeforeClass
-    public static void setUp() throws IOException {
-        server = HttpServer.create(new InetSocketAddress("127.0.0.1", 0), 0);
-        server.createContext("/", new MyHandler());
-        server.setExecutor(null);
-        server.start();
-        String address = server.getAddress().toString().replace("/", "http://");
-        AddressesSettings addressesSettings = AddressesSettings.newBuilder().setEndpoint(address).build();
-        addressesClient = AddressesClient.create(addressesSettings);
+  static class MyHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange t) throws IOException {
+      headers = t.getRequestHeaders();
+      byte[] response = "{\"id\":2000}".getBytes();
+      t.sendResponseHeaders(200, response.length);
+      t.getResponseBody().write(response);
+      t.close();
     }
+  }
 
-    static class MyHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            headers = t.getRequestHeaders();
-            byte[] response = "{\"id\":2000}".getBytes();
-            t.sendResponseHeaders(200, response.length);
-            t.getResponseBody().write(response);
-            t.close();
-        }
-    }
+  @AfterClass
+  public static void tearDown() {
+    server.stop(5);
+  }
 
-    @AfterClass
-    public static void tearDown() {
-        server.stop(5);
-    }
-
-    @Test
-    public void testHeaders() {
-        addressesClient.insert("test", "test", Address.newBuilder().setName("test").build());
-        Assert.assertTrue(headers.get("X-goog-api-client").get(0).contains("rest/"));
-        Assert.assertTrue(headers.get("Content-type").get(0).contains("application/json"));
-    }
-
+  @Test
+  public void testHeaders() {
+    addressesClient.insert("test", "test", Address.newBuilder().setName("test").build());
+    Assert.assertTrue(headers.get("X-goog-api-client").get(0).contains("rest/"));
+    Assert.assertTrue(headers.get("Content-type").get(0).contains("application/json"));
+  }
 }
