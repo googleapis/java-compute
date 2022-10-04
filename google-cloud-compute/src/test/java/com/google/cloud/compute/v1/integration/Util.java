0f12f41b80c4552ce06169308001cb23e9b36b97
Lawrence Qiu <lawrenceqiu@google.com>
2022-10-04 01:38:02 +0000

    chore: Fix Util.java file

--- google-cloud-compute/src/test/java/com/google/cloud/compute/v1/integration/Util.java
+++ google-cloud-compute/src/test/java/com/google/cloud/compute/v1/integration/Util.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.cloud.compute.v1.integration;
 
 import com.google.cloud.compute.v1.Address;
@@ -51,14 +66,12 @@ public class Util {
   }
 
   private static boolean isCreatedBeforeThresholdTime(Instant instant) {
-    return true;
-    // return instant.isBefore(Instant.now().minus(DELETION_THRESHOLD_TIME_HOURS, ChronoUnit.HOURS));
+    return instant.isBefore(Instant.now().minus(DELETION_THRESHOLD_TIME_HOURS, ChronoUnit.HOURS));
   }
 
   private static boolean isCreatedBeforeThresholdTime(String timestamp) {
-    return true;
-    // return OffsetDateTime.parse(timestamp)
-    //     .toInstant()
-    //     .isBefore(Instant.now().minus(DELETION_THRESHOLD_TIME_HOURS, ChronoUnit.HOURS));
+    return OffsetDateTime.parse(timestamp)
+        .toInstant()
+        .isBefore(Instant.now().minus(DELETION_THRESHOLD_TIME_HOURS, ChronoUnit.HOURS));
   }
 }
